ee7b811010e4dce177d8eddf905936f6290c0fb1
jay <116497509@qq.com>
2020-06-06 23:55:35 +0800

    commit new class

---
+++ HelloGit/src/main/java/com/jay/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.jay.git;
+
+/**
+ * @author shkstart
+ * @create 2020-06-06 23:43
+ * @desc
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
